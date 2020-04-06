e2157a0c3cd1b56803cb55ebe82b4f47866028b5
Adam English <englishadam@hotmail.co.uk>
2020-04-06 13:46:55 +0100

    Updated tutorial

--- v5/tutorial/1_App/index.js
+++ v5/tutorial/1_App/index.js
@@ -5,11 +5,18 @@
  * with a fallback to a canvas render. It will also setup the ticker
  * and the root stage PIXI.Container.
  */
-const app = new PIXI.Application();
+const app = new PIXI.Application({ 
+  width: 256,         // default: 800
+  height: 256,        // default: 600
+  antialias: true,    // default: false
+  transparent: false, // default: false
+  resolution: 1       // default: 1
+});
 
 /**
  * Attached <canvas> to body.
- * app.view provides the HTMLCanvasElement
+ * 
+ * app.view provides the HTMLCanvasElement to attach to
  * The application will create a canvas element for you that you
  * can then insert into the DOM.
 */
@@ -43,25 +50,39 @@ document.body.appendChild(app.view);
  * 
  * 
  * -- PROPERTIES
- *      app._options // Default Options
- *      app.loader   // (PIXI.Loader)                         // default -                    // - readoly - Loader instance to help with asset loading.
- *      app.render   // (PIXI.Renderer | PIXI.CanvasRenderer) // default -                    //           - WebGL renderer if available, otherwise CanvasRenderer
- *      app.resizeTo // (Window | HTMLElement)                // default -                    //           - The element or window to resize the application to.
- *      app.screen   // (PIXI.Rectangle)                      // default -                    // - readoly - Reference to the renderer's screen rectangle. Its safe to use as filterArea or hitArea for whole screen
- *      app.stage    // (PIXI.Container)                      // default -                    //           - The root display container that's rendered.
- *      app.ticker   // (PIXI.Ticker)                         // default - PIXI.Ticker.shared //           - Ticker for doing render updates.
- *      app.view     // (HTMLCanvasElement)                   // default -                    // - readoly - Reference to the renderer's canvas element.
+ *      app.loader    // PIXI.Loader                         // default -                    // - readoly - Loader instance to help with asset loading.
+ *      app.render    // PIXI.Renderer | PIXI.CanvasRenderer // default -                    //           - WebGL renderer if available, otherwise CanvasRenderer
+ *      app.resizeTo  // Window | HTMLElement                // default -                    //           - The element or window to resize the application to.
+ *      app.screen    // PIXI.Rectangle                      // default -                    // - readoly - Reference to the renderer's screen rectangle. Its safe to use as filterArea or hitArea for whole screen
+ *      app.stage     // PIXI.Container                      // default -                    //           - The root display container that's rendered.
+ *      app.ticker    // PIXI.Ticker                         // default - PIXI.Ticker.shared //           - Ticker for doing render updates.
+ *      app.view      // HTMLCanvasElement                   // default -                    // - readoly - Reference to the renderer's canvas element.
+ * 
+ * -- STATIC METHODS
+ *      // Register a middleware plugin for the application
+ *      PIXI.Application.registerPlugin({
+ *        plugin // PIXI.Application.Plugin // - Plugin to be installed (See type definition)
+ *      })
  * 
  * -- METHODS
- *      PIXI.Application.registerPlugin({
- *        plugin // (PIXI.Application.Plugin) - Plugin to be installed
- *      })
+ *      // Destroy and don't use after this, able to remove the texture of children sprites and base textures
  *      app.destroy({
  *        removeView,   // boolean          // default - false  // optional - Automatically remove canvas from DOM.
  *        stageOptions  // object | boolean // default -        // optional - Options parameter. A boolean will act as if all options have been set to that value
+ *                                                              //          - {
+ *                                                                              children,   // boolean  // default - false  // optional - if set to true, all the children will have their destroy method called as well. 'stageOptions' will be passed on to those calls.
+ *                                                                              texture,    // boolean  // default - false  // optional - Only used for child Sprites if stageOptions.children is set to true. Should it destroy the texture of the child sprite
+ *                                                                              baseTexture // boolean  // default - false  // optional - Only used for child Sprites if stageOptions.children is set to true. Should it destroy the base texture of the child sprite
+ *                                                                            }
  *      })
+ *      app.queueResize ()       // Queues a resizing of application, similar to resize(). Must have resizeTo set. Resize is throttled, so it's safe to call this multiple times per frame and it'll only be called once.
  *      app.render()             // Render the current stage.
+ *      app.resize ()            // Execute an immediate resize on the renderer, this is not throttled and can be expensive to call many times in a row. Will resize only if resizeTo property is set.
  *      app.start()              // Convenience method for starting the render.
  *      app.stop()               // Convenience method for stopping the render.
+ *
  * -- TYPE DEFINITIONS
+ *    PIXI.Application.Plugin:
+ *      init    // function   // Called when Application is constructed, scoped to Application instance. Passes in options as the only argument, which are Application constructor options.
+ *      destroy // function   // Called when destroying Application, scoped to Application instance
  */
